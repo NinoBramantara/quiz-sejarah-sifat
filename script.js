69114593d44787c3014160a4e7cb99c5fd02d997
NinoBramantara <ninobramantara@gmail.com>
2022-08-27 20:43:34 +0700

    Menambah 10 pertanyaan di dalam script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const quizData = [
         c: "Sumber Sejarah",
         d: "Bentuk Sejarah",
         correct: "c",
-    }
+    },
+
     {
         question: "Segala sesuatu yang berwujud dan tidak berwujud, serta berguna bagi penelitian sejarah Indonesia sejak zaman purba sampai dengan sekarang. Merupakan pernyataan dari...",
         a: "Moh. Ali",
@@ -14,13 +15,78 @@ const quizData = [
         c: "Dr. Sartika",
         d: "S. Soedjojono",
         correct: "a",
-    }
+    },
+    
     {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        d: "",
-        correct: "",
-    }
+        question: "Sumber yang didapat dari orang yang mendengar peristiwa tersebut dari orang lain adalah sumber...",
+        a: "Primer",
+        b: "Sekunder",
+        c: "Tersier",
+        d: "Kuarter",
+        correct: "b",
+    },
+
+    {
+        question: "Sumber sejarah yang berupa rangkuman tekstual adalah sumber...",
+        a: "Primer",
+        b: "Sekunder",
+        c: "Tersier",
+        d: "Kuarter",
+        correct: "c",
+    },
+
+    {
+        question: "Sumber sejarah yang ditulis oleh orang yang mengalami peristiwa secara langsung adalah sumber...",
+        a: "Primer",
+        b: "Sekunder",
+        c: "Tersier",
+        d: "Kuarter",
+        correct: "a",
+    },
+
+    {
+        question: "Salah satu kelebihan sifat primer adalah...",
+        a: "Mudah diakses",
+        b: "Membutuhkan waktu lama",
+        c: "Subjektif",
+        d: "Objektif",
+        correct: "d",
+    },
+
+    {
+        question: "Salah satu kekurangan sekunder adalah...",
+        a: "Sulit diakses",
+        b: "Subjektif",
+        c: "Membutuhkan waktu lama",
+        d: "Biaya yang dikeluarkan cukup besar",
+        correct: "b",
+    },
+
+    {
+        question: "Salah satu contoh dari sumber primer adalah...",
+        a: "Hikayat",
+        b: "Wikipedia",
+        c: "Encyclopedia",
+        d: "Prasasti",
+        correct: "d",
+    },
+
+    {
+        question: "Salah satu contoh dari sumber sekunder adalah...",
+        a: "Hikayat",
+        b: "Wikipedia",
+        c: "Encyclopedia",
+        d: "Prasasti",
+        correct: "a",
+    },
+
+    {
+        question: "Hikayat, buku sejarah, dan artikel ilmiah merupakan contoh dari sumber...",
+        a: "Primer",
+        b: "Sekunder",
+        c: "Tersier",
+        d: "Kuarter",
+        correct: "b",
+    },
 ]
+
