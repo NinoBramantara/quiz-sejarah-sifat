b9dd03f9246629ee8c9a0ece32aadb9ca143967f
NinoBramantara <ninobramantara@gmail.com>
2022-08-27 13:12:43 +0700

    Menambah pertanyaan di dalam script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const quizData = [
+    {
+        question: "Segala sesuatu yang berwujud dan tidak berwujud, serta berguna bagi penelitian sejarah Indonesia sejak zaman purba sampai dengan sekarang. Merupakan pengertian dari...",
+        a: "Sejarah",
+        b: "Sifat Sejarah",
+        c: "Sumber Sejarah",
+        d: "Bentuk Sejarah",
+        correct: "c",
+    }
+    {
+        question: "Segala sesuatu yang berwujud dan tidak berwujud, serta berguna bagi penelitian sejarah Indonesia sejak zaman purba sampai dengan sekarang. Merupakan pernyataan dari...",
+        a: "Moh. Ali",
+        b: "Abdul Muis",
+        c: "Dr. Sartika",
+        d: "S. Soedjojono",
+        correct: "a",
+    }
+    {
+        question: "",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        correct: "",
+    }
+]
